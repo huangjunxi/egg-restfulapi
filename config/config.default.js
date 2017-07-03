84a60137ef1ede8ce500420900b9d1bff265ef37
JK\huangjunxi <764503544@qq.com>
2017-07-03 16:23:21 +0800

    this is my commit project from localhost

---
+++ config/config.default.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // should change to your own
+  config.keys = appInfo.name + '_1498703032058_6602';
+
+  // add your config here
+  exports.keys ="safeCookie";
+
+  return config;
+};
